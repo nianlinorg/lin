b4878771d4fa053dba9164ef6bd9b05803207477
nianliner <876395565@qq.com>
2017-06-23 09:47:36 +0800

    dev update

--- src/main/java/fun/Dev.java
+++ src/main/java/fun/Dev.java
@@ -1,7 +1,7 @@
 package fun;
 
-public class Fun {
+public class Dev {
 	public static void main(String[] args) {
-		System.out.println("-dev-change--");
+		System.out.println("--this is dev---");
 	}
 }
